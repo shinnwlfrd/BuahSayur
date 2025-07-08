42a9bf4c9570f3ca586b889fee32402208e7f1a4
Putra-vncz <vnczrdsrg@gmail.com>
2025-07-08 10:07:52 +0700

    Delete model/convert_to_tflite.py

--- model/convert_to_tflite.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-
-model = tf.keras.models.load_model("model_buah_keras.h5")
-
-converter = tf.lite.TFLiteConverter.from_keras_model(model)
-tflite_model = converter.convert()
-
-with open("model_buah.tflite", "wb") as f:
-    f.write(tflite_model)
